8abadc90696f2b3e411c023aaf9bc8fa8fa2cb41
Precious Ajilore <precious.ajilore04@gmail.com>
2026-02-01 22:33:37 -0700

    Add assignment 3 starter files and templates
    Added initial files for CMPUT 331 Assignment 3, including problem statement (a3.txt), starter code for problems 2 and 5 (a3p2.py, a3p5.py), and a3.pdf. These files provide templates and test scaffolding for student solutions.

---
+++ a3p2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+#---------------------------------------------------------------
+#
+# CMPUT 331 Student Submission License
+# Version 1.0
+# Copyright 2026 <<Insert your name here>>
+#
+# Redistribution is forbidden in all circumstances. Use of this software
+# without explicit authorization from the author is prohibited.
+#
+# This software was produced as a solution for an assignment in the course
+# CMPUT 331 - Computational Cryptography at the University of
+# Alberta, Canada. This solution is confidential and remains confidential 
+# after it is submitted for grading.
+#
+# Copying any part of this solution without including this copyright notice
+# is illegal.
+#
+# If any portion of this software is included in a solution submitted for
+# grading at an educational institution, the submitter will be subject to
+# the sanctions for plagiarism at that institution.
+#
+# If this software is found in any public website or public repository, the
+# person finding it is kindly requested to immediately report, including 
+# the URL or other repository locating information, to the following email
+# address:
+#
+#          gkondrak <at> ualberta.ca
+#
+#---------------------------------------------------------------
+
+"""
+CMPUT 331 Assignment 3 Student Solution
+January 2026
+Author: <Your name here>
+"""
+
+def affine_key_count(m):
+    raise NotImplementedError
+    
+
+
+
+def test():
+    assert affine_key_count(65) == 3119
+    
+
+from sys import flags
+
+if __name__ == "__main__" and not flags.interactive:
+    test()
